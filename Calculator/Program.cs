89aa9a3dc48763039a8aa094afa538451adf7ec9
Devin Cunningham <dtyler.cunningham@gmail.com>
2018-10-23 19:55:50 -0700

    working

---
+++ Calculator/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum;
+            int secondNum;
+            string op = "";
+            int result;
+
+            Console.Write("Enter Your First Number: ");
+            firstNum = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter Your Operator: + - / * ");
+            op = Console.ReadLine();
+
+            Console.Write("Choose Your Second Number: ");
+            secondNum = Convert.ToInt32(Console.ReadLine());
+
+            switch (op)
+            {
+                case "+":
+                    result = firstNum + secondNum;
+                    Console.WriteLine(result);
+                    break;
+                case "-":
+                    result = firstNum - secondNum;
+                    Console.WriteLine(result);
+                    break;
+                case "*":
+                    result = firstNum * secondNum;
+                    Console.WriteLine(result);
+                    break;
+                case "/":
+                    result = firstNum / secondNum;
+                    Console.WriteLine(result);
+                    break;
+                default:
+                    Console.WriteLine("Default case");
+                    break;
+            }
+        }
+    }
+}
